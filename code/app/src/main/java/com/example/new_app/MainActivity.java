758a3eef5313a1922167af52a192bdbfb7b55172
ela_71 <ela_71@localhost>
2019-03-20 14:48:37 +0600

    add code

---
+++ code/app/src/main/java/com/example/new_app/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.new_app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+private Button joinNowButton,loginButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        joinNowButton=findViewById(R.id.main_join_btn);
+        loginButton=findViewById(R.id.main_login_btn);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent (MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        joinNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent (MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
