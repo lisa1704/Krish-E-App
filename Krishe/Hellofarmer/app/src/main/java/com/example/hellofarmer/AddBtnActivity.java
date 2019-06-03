c298e260bbd15925d4f28c78d891b8dd7233a1c6
ela_71 <ela_71@localhost>
2019-06-03 16:08:17 +0600

    # new app

---
+++ Krishe/Hellofarmer/app/src/main/java/com/example/hellofarmer/AddBtnActivity.java
@@ -0,0 +1,35 @@
+package com.example.hellofarmer;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AddBtnActivity extends AppCompatActivity {
+
+    private Button addPro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_btn);
+        addPro=(Button)findViewById(R.id.product);
+        addPro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openprodactivity();
+            }
+        });
+
+    };
+    public void openprodactivity()
+    {
+        Intent intent=new Intent(this,addNewProductActivity.class);
+        startActivity(intent);
+
+    }
+
+
+}
+
