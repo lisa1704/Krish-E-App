650658ea818dbe88927254fbe00fe5af2de56884
Rodoshe <Rodoshe@localhost>
2019-06-08 16:39:38 +0600

    1

---
+++ Krishe/Hellofarmer/app/src/main/java/com/example/hellofarmer/Oil.java
@@ -0,0 +1,14 @@
+package com.example.hellofarmer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Oil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oil);
+    }
+}
