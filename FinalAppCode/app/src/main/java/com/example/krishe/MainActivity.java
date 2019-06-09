3877f765389e4befb4f0f260870055e264f00b9f
ela_71 <ela_71@localhost>
2019-06-09 23:30:45 +0600

    #final app commit 1

---
+++ FinalAppCode/app/src/main/java/com/example/krishe/MainActivity.java
@@ -0,0 +1,156 @@
+package com.example.krishe;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridLayout;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private DrawerLayout drawer;
+    NavigationView navigationView;
+    GridLayout mainGrid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        mainGrid = (GridLayout) findViewById(R.id.mainGrid);
+
+        //Set Event
+        setSingleEvent(mainGrid);
+        //setToggleEvent(mainGrid);
+    }
+
+
+
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.myprofile:
+                    startActivity(new Intent(this, Crops.class));
+                    break;
+                case R.id.granery:
+                    startActivity(new Intent(this, Crops.class));
+                    break;
+                case R.id.garage:
+                    startActivity(new Intent(this, Crops.class));
+                    break;
+                case R.id.Shoppingcart:
+                    startActivity(new Intent(this, Crops.class));
+                    break;
+                case R.id.RentedMachinery:
+                    startActivity(new Intent(this, Crops.class));
+                    break;
+                case R.id.logout:
+
+                    break;
+
+            }
+
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        }
+  /*  private void logout(){
+        firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+*/
+        @Override
+        public void onBackPressed() {
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            } else {
+                super.onBackPressed();
+            }
+        }
+
+
+
+    private void setToggleEvent(GridLayout mainGrid) {
+        //Loop all child item of Main Grid
+        for (int i = 0; i < mainGrid.getChildCount(); i++) {
+            //You can see , all child item is CardView , so we just cast object to CardView
+            final CardView cardView = (CardView) mainGrid.getChildAt(i);
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (cardView.getCardBackgroundColor().getDefaultColor() == -1) {
+                        //Change background color
+                        cardView.setCardBackgroundColor(Color.parseColor("#FF6F00"));
+                        Toast.makeText(MainActivity.this, "State : True", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        //Change background color
+                        cardView.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+                        Toast.makeText(MainActivity.this, "State : False", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void setSingleEvent(GridLayout mainGrid) {
+        //Loop all child item of Main Grid
+
+        //You can see , all child item is CardView , so we just cast object to CardView
+        CardView rentcardView = (CardView) findViewById(R.id.RentSearch);
+        CardView cropcardView = (CardView) findViewById(R.id.cropsearch);
+
+        rentcardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,FilterActivity.class);
+
+                startActivity(intent);
+
+            }
+        });
+        cropcardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,FilterActivity.class);
+
+                startActivity(intent);
+
+            }
+        });
+    }
+
+
+
+}
+
--- Krishe/Krishe_final/app/src/main/java/com/example/krishe/Welcome.java
+++ Krishe/Krishe_final/app/src/main/java/com/example/krishe/Welcome.java
@@ -1,213 +1,123 @@
 package com.example.krishe;
 
-import android.content.Context;
+
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-
 public class Welcome extends AppCompatActivity {
-    private ViewPager viewPager;
-    private MyViewPagerAdapter myViewPagerAdapter;
-    private LinearLayout dotsLayout;
-    private TextView[] dots;
-    private int[] layouts;
-    private Button btnSkip, btnNext;
-    private UserSession prefManager;
+    private ViewPager msLideViewPager;
+    private LinearLayout mDoLayout;
+    private SlideAdapter sliderAdapter;
+    private TextView[] mdots;
+
+    private Button mNextButton;
+    private Button mBackButton;
+    private int mcurrentpage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Checking for first time launch - before calling setContentView()
-        prefManager = new UserSession(this);
-        if (!prefManager.isFirstTimeLaunch()) {
-            launchHomeScreen();
-            finish();
-        }
-
-        UserSession userSession = new UserSession(this);
-
-
-        // Making notification bar transparent
-        if (Build.VERSION.SDK_INT >= 21) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-
         setContentView(R.layout.activity_welcome);
-
-        viewPager = (ViewPager) findViewById(R.id.view_pager);
-        dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
-        btnSkip = (Button) findViewById(R.id.btn_skip);
-        btnNext = (Button) findViewById(R.id.btn_next);
-
-
-        // layouts of all welcome sliders
-        // add few more layouts if you want
-        layouts = new int[]{
-                R.layout.welcome_slide1,
-                R.layout.welcome_slide2,
-                R.layout.welcome_slide3,
-                R.layout.welcome_slide4};
-
-        // adding bottom dots
-        addBottomDots(0);
-
-        // making notification bar transparent
-        changeStatusBarColor();
-
-        myViewPagerAdapter = new MyViewPagerAdapter();
-        viewPager.setAdapter(myViewPagerAdapter);
-        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
-
-        btnSkip.setOnClickListener(new View.OnClickListener() {
+        msLideViewPager=(ViewPager) findViewById(R.id.slideview);
+        mDoLayout=(LinearLayout)findViewById(R.id.dots);
+        mNextButton=(Button)findViewById(R.id.nextBtn);
+        mBackButton=(Button)findViewById(R.id.prevbtn);
+        sliderAdapter=new SlideAdapter( this);
+        msLideViewPager.setAdapter(sliderAdapter);
+        addDotsIndicator(0);
+        msLideViewPager.addOnPageChangeListener(viewListener);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                launchHomeScreen();
+                msLideViewPager.setCurrentItem(mcurrentpage+1);
             }
         });
-
-        btnNext.setOnClickListener(new View.OnClickListener() {
+        mBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // checking for last page
-                // if last page home screen will be launched
-                int current = getItem(+1);
-                if (current < layouts.length) {
-                    // move to next screen
-                    viewPager.setCurrentItem(current);
-                } else {
-                    launchHomeScreen();
-                }
+                msLideViewPager.setCurrentItem(mcurrentpage-1);
             }
         });
     }
 
-    private void addBottomDots(int currentPage) {
-        dots=new TextView[4];
-        dotsLayout.removeAllViews();
-        for(int i=0;i<dots.length;i++)
+    public void addDotsIndicator(int position)
+    {
+        mdots=new TextView[4];
+        mDoLayout.removeAllViews();
+        for(int i=0;i<mdots.length;i++)
         {
-            dots[i]=new TextView(this);
-            dots[i].setText(Html.fromHtml("&#8226;"));
-            dots[i].setTextSize(35);
-            dots[i].setTextColor(getResources().getColor(R.color.white));
+            mdots[i]=new TextView(this);
+            mdots[i].setText(Html.fromHtml("&#8226;"));
+            mdots[i].setTextSize(35);
+            mdots[i].setTextColor(getResources().getColor(R.color.white));
 
-            dotsLayout.addView(dots[i]);
+            mDoLayout.addView(mdots[i]);
 
 
         }
-        if(dots.length>0)
+        if(mdots.length>0)
         {
-            dots[currentPage].setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+            mdots[position].setTextColor(getResources().getColor(R.color.colorPrimaryDark));
         }
     }
 
-    private int getItem(int i) {
-        return viewPager.getCurrentItem() + i;
-    }
 
-    private void launchHomeScreen() {
-        prefManager.setFirstTimeLaunch(false);
-        if(prefManager.isLoggedIn()) {
-            Intent i = new Intent(Welcome.this, MainActivity.class);
-            startActivity(i);
-        }else{
-            Intent i = new Intent(Welcome.this, Login.class);
-            startActivity(i);
+    ViewPager.OnPageChangeListener viewListener=new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
         }
-        finish();
-    }
-
-    //  viewpager change listener
-    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
 
         @Override
-        public void onPageSelected(int position) {
-            addBottomDots(position);
+        public void onPageSelected(int i) {
+            addDotsIndicator(i);
 
-            // changing the next button text 'NEXT' / 'GOT IT'
-            if (position == layouts.length - 1) {
-                // last page. make button text to GOT IT
-                btnNext.setText(getString(R.string.start));
-                btnSkip.setVisibility(View.GONE);
-            } else {
-                // still pages are left
-                btnNext.setText(getString(R.string.next));
-                btnSkip.setVisibility(View.VISIBLE);
+            mcurrentpage=i;
+            if(i==0)
+            {
+                mNextButton.setEnabled(true);
+                mBackButton.setEnabled(false);
+                mBackButton.setVisibility(View.INVISIBLE);
+                mNextButton.setText("Next");
+                mBackButton.setText("");
+            } else if(i==mdots.length-1)
+            {
+                mNextButton.setEnabled(true);
+                mBackButton.setEnabled(true);
+                mBackButton.setVisibility(View.VISIBLE);
+                mNextButton.setText("Finish");
+                mBackButton.setText("Back");
+                mNextButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent =new Intent(Welcome.this,Login.class);
+                        startActivity(intent);
+                    }
+                });
+
             }
+            else
+            {
+                mNextButton.setEnabled(true);
+                mBackButton.setEnabled(true);
+                mBackButton.setVisibility(View.VISIBLE);
+                mNextButton.setText("Next");
+                mBackButton.setText("Back");
+            }
+
         }
 
         @Override
-        public void onPageScrolled(int arg0, float arg1, int arg2) {
-
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int arg0) {
+        public void onPageScrollStateChanged(int i) {
 
         }
     };
-
-    /**
-     * Making notification bar transparent
-     */
-    private void changeStatusBarColor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-        }
-    }
-
-    /**
-     * View pager adapter
-     */
-    public class MyViewPagerAdapter extends PagerAdapter {
-        private LayoutInflater layoutInflater;
-
-        public MyViewPagerAdapter() {
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-            View view = layoutInflater.inflate(layouts[position], container, false);
-            container.addView(view);
-
-            return view;
-        }
-
-        @Override
-        public int getCount() {
-            return layouts.length;
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object obj) {
-            return view == obj;
-        }
-
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            View view = (View) object;
-            container.removeView(view);
-        }
-    }
 }
 
